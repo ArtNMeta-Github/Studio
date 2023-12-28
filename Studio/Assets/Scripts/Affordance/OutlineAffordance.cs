37af04d6f429f1dcca9f573a3256a253d2d8edb9
gh1928 <oos1928@gmail.com>
2023-12-28 15:44:23 +0900

    라이도 인터렉트 기능 추가 / 스프레이 앞면으로 잡히게 변경

--- Studio/Assets/Scripts/Affordance/OutlineAffordance.cs
+++ Studio/Assets/Scripts/Affordance/OutlineAffordance.cs
@@ -5,11 +5,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class OutlineAffordance : Outline
 {
-    public XRGrabInteractable interactable;
+    public XRBaseInteractable interactable;
     public override void Start()
     {
         base.Start();
-        interactable ??= GetComponent<XRGrabInteractable>();
+        interactable ??= GetComponent<XRBaseInteractable>();
 
         interactable.firstHoverEntered.AddListener(x => enabled = true);
         interactable.lastHoverExited.AddListener(x => enabled = false);
--- Studio/Assets/Scripts/Props/RadioButton.cs
+++ Studio/Assets/Scripts/Props/RadioButton.cs
@@ -2,20 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class RadioButton : MonoBehaviour
 {
-    public UnityEvent evt;    
+    public float pushTime = 0.1f;
+    public float pushDepth = 0.00005f;
+    public UnityEvent evt;
 
-    private void OnTriggerEnter(Collider other)
+    Coroutine buttonPushCoroutine;
+    XRSimpleInteractable simpleInteractable;
+    AudioSource audioSource;
+
+    private void Start()
     {
+        simpleInteractable = GetComponent<XRSimpleInteractable>();
+        simpleInteractable.selectEntered.AddListener(x => StartPushcoroutine());
 
-        if (other.CompareTag("Hand"))
-            evt.Invoke();
+        audioSource = GetComponent<AudioSource>();        
+    }
+    public void StartPushcoroutine()
+    {
+        if (buttonPushCoroutine != null)
+            return;
+
+        evt.Invoke();
+        audioSource.Play();
+        buttonPushCoroutine = StartCoroutine(PushCoroutine());
     }
 
-    private void OnDestroy()
+    IEnumerator PushCoroutine()
     {
-        evt.RemoveAllListeners();
+        float timer = 0f;
+        float invPushTime = 1 / pushTime;
+        Vector3 newPos = Vector3.zero;
+
+        bool isPushing = false;
+
+        while (true)
+        {
+            if(!isPushing)
+            {
+                if (timer > pushTime) isPushing = true;
+
+                timer += Time.deltaTime;                
+            }
+
+            if(isPushing)
+            {
+                if (timer < 0f) break;
+
+                timer -= Time.deltaTime;                
+            }                
+
+            newPos.x = Mathf.Lerp(0f, pushDepth, timer * invPushTime);
+            transform.localPosition = newPos;
+            yield return null;
+        }
+
+        transform.localPosition = Vector3.zero;
+
+        buttonPushCoroutine = null;
+        yield break;
     }
 }
