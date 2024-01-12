801d9af912e660dda21aee9d6845309301344769
gh1928 <oos1928@gmail.com>
2024-01-12 15:45:17 +0900

    마블링 페인트기능 추가

---
+++ Studio/Assets/Scripts/Marbling/MarblingShaderController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarblingShaderController : MonoBehaviour
+{
+    Slider slider;
+
+    public MablingShaderData shaderData;
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+
+        slider.onValueChanged.AddListener(shaderData.SetDistortionValue);
+    }
+}
